c78f6f2ae9b9c42def9933615db5373da720baa8
Sedrak Adoyan <set.adoyan@gmail.com>
2023-11-04 21:56:47 +0400

    update projects

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 import {
   Card,
@@ -8,135 +8,113 @@ import {
   CardActions,
   Typography,
   Button,
-  Zoom
-} from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import PreviewIcon from '@mui/icons-material/Preview';
-import { styled, alpha } from '@mui/material/styles';
+  Zoom,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import PreviewIcon from "@mui/icons-material/Preview";
+import { styled, alpha } from "@mui/material/styles";
 
 const CardBody = styled(Card)(({ theme }) => ({
-  position: 'relative',
-  height: '100%',
+  position: "relative",
+  height: "100%",
   background: alpha(theme.palette.common.black, 0.6),
-  color: theme.palette.common.white
+  color: theme.palette.common.white,
 }));
 
-const Media = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Media = styled("div")(({ theme }) => ({
+  position: "relative",
   height: 225,
 }));
 
 const CardPreview = styled(CardMedia)(({ theme }) => ({
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 'auto',
-  height: '100%'
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "auto",
+  height: "100%",
 }));
 
 const CardOverlay = styled(Card)(({ theme }) => ({
-  position: 'absolute',
+  position: "absolute",
   top: 0,
   left: 0,
-  width: '100%',
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
   background: alpha(theme.palette.common.black, 0.9),
-  color: theme.palette.common.white
+  color: theme.palette.common.white,
 }));
 
-
 const OverlayContent = styled(CardContent)(({ theme }) => ({
-  height: '100%',
-  padding: '5px 15px 0',
-  marginBottom: -15
+  height: "100%",
+  padding: "5px 15px 0",
+  marginBottom: -15,
 }));
 
 const Actions = styled(CardActions)(({ theme }) => ({
-  padding: '14px 15px',
-  display: 'flex',
-  justifyContent: 'space-around',
-  background: theme.palette.common.black
+  padding: "14px 15px",
+  display: "flex",
+  justifyContent: "space-around",
+  background: theme.palette.common.black,
 }));
 
 const ProjectCard = ({ project }) => {
   const [isOverlay, setIsOverlay] = React.useState(false);
   return (
     <CardBody
-      onMouseEnter={e => setIsOverlay(true)}
-      onMouseLeave={e => setIsOverlay(false)}
+      onMouseEnter={(e) => setIsOverlay(true)}
+      onMouseLeave={(e) => setIsOverlay(false)}
     >
       <Media>
-        <CardPreview
-          component='img'
-          image={project.img}
-        />
+        <CardPreview component="img" image={project.img} />
       </Media>
 
       <CardContent>
-        <Typography
-          variant='h5'
-          component='h3'
-        >
+        <Typography variant="h5" component="h3">
           {project.title}
         </Typography>
       </CardContent>
 
-
-      <Zoom
-        in={isOverlay}
-      >
+      <Zoom in={isOverlay}>
         <CardOverlay>
           <CardHeader
             title={project.title}
-            subheader={
-              <Typography
-                component='div'
-              >
-                {project.type}
-              </Typography>
-            }
+            subheader={<Typography component="div">{project.type}</Typography>}
           />
 
           <OverlayContent>
-            <Typography
-              paragraph
-            >
-              {project.description}
-            </Typography>
+            <Typography paragraph>{project.description}</Typography>
 
-            <Typography
-              component='h4'
-            >
+            <Typography component="h4">
               <b>Стэк:</b>
             </Typography>
 
-            <Typography>
-              {project.stack}
-            </Typography>
+            <Typography>{project.stack}</Typography>
           </OverlayContent>
 
           <Actions>
             <Button
-              size='small'
-              variant='string'
+              size="small"
+              variant="string"
               startIcon={<PreviewIcon />}
               href={project.preview}
-              target='_blank'
+              target="_blank"
             >
               Превью
             </Button>
-            <Button
-              size='small'
-              variant='string'
-              startIcon={<GitHubIcon />}
-              href={project.github}
-              target='_blank'
-            >
-              Код
-            </Button>
+            {project.github && (
+              <Button
+                size="small"
+                variant="string"
+                startIcon={<GitHubIcon />}
+                href={project.github}
+                target="_blank"
+              >
+                Код
+              </Button>
+            )}
           </Actions>
         </CardOverlay>
       </Zoom>
