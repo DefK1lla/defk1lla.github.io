b418fd6b162ebc5da8e7e165abe0e3627d2b4a21
Sedrak Adoyan <set.adoyan@gmail.com>
2023-11-19 22:56:25 +0400

    update content

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,53 +1,50 @@
-import { Container, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Container, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 import Marquee from "react-fast-marquee";
 
-import SectionTitle from '../components/SectionTitle';
-import Accent from '../components/Accent';
+import SectionTitle from "../components/SectionTitle";
+import Accent from "../components/Accent";
 
-const Wrapper = styled('section')(({ theme }) => ({
+const Wrapper = styled("section")(({ theme }) => ({
   background: theme.palette.common.white,
 }));
 
-const Content = styled('div')(({ theme }) => ({
-  padding: '50px 0'
+const Content = styled("div")(({ theme }) => ({
+  padding: "50px 0",
 }));
 
 const Text = styled(Typography)(({ theme }) => ({
   maxWidth: 600,
-  margin: '0 auto 50px',
-  textAlign: 'center'
+  margin: "0 auto 50px",
+  textAlign: "center",
 }));
 
-const MarqueeTrack = styled('div')(({ theme }) => ({
-  padding: '10px 0',
-  background: theme.palette.primary.main
+const MarqueeTrack = styled("div")(({ theme }) => ({
+  padding: "10px 0",
+  background: theme.palette.primary.main,
 }));
 
-const MarqueeText = styled('span')(({ theme }) => ({
-  display: 'inline-block',
-  margin: '5px 30px',
-  fontSize: '1.4rem',
-  fontStyle: 'italic',
-  color: theme.palette.common.white
+const MarqueeText = styled("span")(({ theme }) => ({
+  display: "inline-block",
+  margin: "5px 30px",
+  fontSize: "1.4rem",
+  fontStyle: "italic",
+  color: theme.palette.common.white,
 }));
 
 const About = () => {
   return (
-    <Wrapper
-      id='about'
-    >
+    <Wrapper id="about">
       <Container>
         <Content>
-          <SectionTitle
-            component='h2'
-          >
-            Обо мне
-          </SectionTitle>
+          <SectionTitle component="h2">Обо мне</SectionTitle>
 
           <Text>
-            Привет! Меня зовут Адоян Седрак и я веб-разработчик. Окончил грузинский технический университет по специальности	&#171;Информатика&#187;. Ищу работу в компании, чтобы получить опыт командной разработки. Выполняю поставленные задачи качественно и в срок. Пролистайте страницу ниже, чтобы увидеть мои работы. <br />
+            Привет! Меня зовут Адоян Седрак и я веб-разработчик. Мой опыт работы
+            недавно перевалил за три года, но я продолжаю учиться, чтобы
+            внедрять что-то новое, полезное и уникальное в проекты, над которыми
+            работаю. Пролистайте страницу ниже, чтобы увидеть мои работы. <br />
             <Accent>Для меня важно, чтобы клиент остался доволен.</Accent>
           </Text>
         </Content>
@@ -58,34 +55,26 @@ const About = () => {
           pauseOnHover={true}
           gradient={false}
           speed={20}
-          direction='left'
+          direction="left"
         >
           <MarqueeText>
             Самомотивирован и всегда готов совершенствоваться.
           </MarqueeText>
 
-          <MarqueeText>
-            Держу себя в курсе последних технологий.
-          </MarqueeText>
+          <MarqueeText>Держу себя в курсе последних технологий.</MarqueeText>
 
-          <MarqueeText>
-            Развиваю свои навыки в различнных областях.
-          </MarqueeText>
+          <MarqueeText>Развиваю свои навыки в различнных областях.</MarqueeText>
         </Marquee>
 
         <Marquee
           pauseOnHover={true}
           gradient={false}
           speed={20}
-          direction='right'
+          direction="right"
         >
-          <MarqueeText>
-            Готов приступить к работе прямо сейчас!
-          </MarqueeText>
+          <MarqueeText>Готов приступить к работе прямо сейчас!</MarqueeText>
 
-          <MarqueeText>
-            Сделаю все качественно и в указанный срок!
-          </MarqueeText>
+          <MarqueeText>Сделаю все качественно и в указанный срок!</MarqueeText>
 
           <MarqueeText>
             Моя цель - предоставлять лучшую услугу на рынке!
